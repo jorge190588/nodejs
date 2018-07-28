006eec1308e7d9b8aa5951779f5410cfe17e4b92
jorge190588 <netneill@hotmail.com>
2018-07-28 08:59:07 -0600

    check if form is valid

--- public/js/alumno/nuevo.js
+++ public/js/alumno/nuevo.js
@@ -1,18 +1,40 @@
 var ModuloNuevo = function(){
 	var _private = {}, _public = {}; 
-
+	_private.formulario=null;
 	_public.__construct = function() {
 		return _public;
 	};
 
 	_public.iniciar=function(){
+		_private.asignarFormulario();
 		_private.agregarEventoAbotonGuardar();
+	}
+
+	_private.asignarFormulario=function(){
+		var elementos = $("form");
+		if (elementos.length==0){
+			console.log("formulario de nuevo no encontrado");
+		}else{
+			_private.formulario=elementos[0];
+		}
 	}
 
 	_private.agregarEventoAbotonGuardar=function(){
 		$("#guardar")[0].addEventListener('click', function(event) { 
-			console.log("guardar")
+			_private.validarFormulario();			
 		});
+	}
+
+	_private.validarFormulario=function(){
+		var esFormularioValido=_private.formulario.checkValidity();
+		console.log("formularios es valido ",esFormularioValido);
+		var elementos = $("form > div > input");
+		var esElementoValido=false;
+		for(var index=0;index< elementos.length;index++){
+			esElementoValido=elementos[index].checkValidity();
+			if (!esElementoValido)
+				console.log("el contenido del elemento ",elementos[index].id," no es valido");
+		}
 	}
 
 	return _public.__construct.apply(this, arguments);
